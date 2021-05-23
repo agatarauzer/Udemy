1ef5a9d5e336ae3889fb47e9446039c266f33f50
agatarauzer <agatarauzer@gmail.com>
2021-05-23 17:03:46 +0200

    Practice Activity #1 - Inversion of Control with XML Configuration

---
+++ SpringAndHibernateForBeginners/spring-demo-one/src/com/luv2code/springdemo/ValleyballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class ValleyballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 1,5 hour on training";
+	}
+	
+}
