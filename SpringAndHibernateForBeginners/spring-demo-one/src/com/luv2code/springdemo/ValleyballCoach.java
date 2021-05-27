762ca57cbea9eda7e49fb0bb3d98d98e98a4b71b
agatarauzer <agatarauzer@gmail.com>
2021-05-27 22:29:18 +0200

    Practice Activity #3 - Bean Scopes with XML Configuration

--- SpringAndHibernateForBeginners/spring-demo-one/src/com/luv2code/springdemo/ValleyballCoach.java
+++ SpringAndHibernateForBeginners/spring-demo-one/src/com/luv2code/springdemo/ValleyballCoach.java
@@ -3,11 +3,15 @@ package com.luv2code.springdemo;
 public class ValleyballCoach implements Coach {
 
 	private FortuneService fortuneService;
-
+	
 	public ValleyballCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
-
+	
+	public ValleyballCoach( ) {
+		
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 1,5 hour on training";
@@ -16,5 +20,5 @@ public class ValleyballCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return "Just saying: " + fortuneService.getFortune();
-	}
+	}	
 }
